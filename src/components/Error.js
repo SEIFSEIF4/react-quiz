54b8c4851435dd1aa75a8db3d9654d1ece73eadd
Seif Elesllam <seifsaif151@gmail.com>
2024-05-04 21:11:44 +0300

    update package.json

--- src/components/Error.js
+++ src/components/Error.js
@@ -2,6 +2,8 @@ function Error() {
   return (
     <p className="error">
       <span>💥</span> There was an error fetching questions.
+      <br />
+      Run <code> npm run server</code>
     </p>
   );
 }
